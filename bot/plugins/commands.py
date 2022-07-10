906851c59e9b8972b704a767ce1fd48e6e9702a2
Adish <100956493+Adishsuresh@users.noreply.github.com>
2022-07-10 13:02:59 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/LoVe_MySeLf_168"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/LoVe_MySeLf_168'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/LoVe_MySeLf_168')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/LoVe_MySeLf_168')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
